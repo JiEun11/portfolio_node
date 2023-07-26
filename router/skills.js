959d9caf8a65776c681e19a11c4b966ba9f00cb3
Bella <shdudtnr3939@naver.com>
2023-07-26 21:04:52 +0900

    [modified] async await 적용

--- router/skills.js
+++ router/skills.js
@@ -10,69 +10,71 @@ const router = express.Router();
 
 // GET /skills
 // GET /skills?categoryId=:categoryId
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const categoryId = req.query.categoryId;
   if (categoryId) {
-    return db
-      .execute(`${SELECT_JOIN} WHERE categoryId=? ${ORDER_DESC}`, [categoryId])
-      .then((result) => {
-        const skills = result[0];
-        skills.length === 0
-          ? res
-              .status(404)
-              .json({ message: `Category id(${id}) is not found !!!` })
-          : res.status(200).json(result[0]);
-      });
+    const result = await db.execute(
+      `${SELECT_JOIN} WHERE categoryId=? ${ORDER_DESC}`,
+      [categoryId]
+    );
+    if (result) {
+      const skills = result[0];
+      skills.length === 0
+        ? res
+            .status(404)
+            .json({ message: `Category id(${id}) is not found !!!` })
+        : res.status(200).json(result[0]);
+    }
   } else {
-    return db
-      .execute(`${SELECT_JOIN} ${ORDER_DESC}`)
-      .then((result) => res.status(200).json(result[0]));
+    const result = await db.execute(`${SELECT_JOIN} ${ORDER_DESC}`);
+    res.status(200).json(result[0]);
   }
 });
 // GET /skills/:id
-router.get("/:id", (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   const id = req.params.id;
-  return db.execute(`${SELECT_JOIN} WHERE sk.id=?`, [id]).then((result) => {
-    const skill = result[0][0];
-    skill
-      ? res.status(200).json(result[0][0])
-      : res.status(404).json({ message: `Skill id(${id}) is not found !!!` });
-  });
+  // return db.execute(`${SELECT_JOIN} WHERE sk.id=?`, [id]).then((result) => {
+  //   const skill = result[0][0];
+  //   skill
+  //     ? res.status(200).json(result[0][0])
+  //     : res.status(404).json({ message: `Skill id(${id}) is not found !!!` });
+  // });
+
+  const result = await db.execute(`${SELECT_JOIN} WHERE sk.id=?`, [id]);
+  const skill = result[0][0];
+  skill
+    ? res.status(200).json(result[0][0])
+    : res.status(404).json({ message: `Skill id(${id}) is not found !!!` });
 });
 
 // POST /skills
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const { categoryId, name } = req.body;
-  return db
-    .execute(
-      "INSERT INTO skills (name, categoryId, createdAt, lastUpdatedAt) VALUES(?,?,?,?)",
-      [name, categoryId, new Date(), new Date()]
-    )
-    .then((result) => res.status(201).json(result));
+  const result = await db.execute(
+    "INSERT INTO skills (name, categoryId, createdAt, lastUpdatedAt) VALUES(?,?,?,?)",
+    [name, categoryId, new Date(), new Date()]
+  );
+  res.status(201).json(result);
 });
 
 // PUT /skills/:id
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   const id = req.params.id;
   const name = req.body.name;
-  return db
-    .execute("UPDATE skills SET name=?, lastUpdatedAt=? WHERE id=?", [
-      name,
-      new Date(),
-      id,
-    ])
-    .then((result) => {
-      result[0].changedRows === 1
-        ? res.status(200).json(result)
-        : res.status(404).json({ message: `Skill id(${id}) is not found!!!` });
-    });
+  const result = await db.execute(
+    "UPDATE skills SET name=?, lastUpdatedAt=? WHERE id=?",
+    [name, new Date(), id]
+  );
+
+  result[0].changedRows === 1
+    ? res.status(200).json(result)
+    : res.status(404).json({ message: `Skill id(${id}) is not found!!!` });
 });
 
 // DELETE /skills/:id
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
-  return db
-    .execute("DELETE FROM skills WHERE id=?", [id])
-    .then((result) => res.sendStatus(204));
+  const result = db.execute("DELETE FROM skills WHERE id=?", [id]);
+  res.sendStatus(204);
 });
 export default router;
