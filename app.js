95a196c0cb7f147c865f0478a3c927c6a479044b
Bella <shdudtnr3939@naver.com>
2023-07-29 16:13:36 +0900

    [add] app.js url encoded 미들웨어 추가

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use(cors());
 // app.use(morgan("tiny"));
 
+app.use(express.urlencoded({ extended: true })); // application-
 app.use("/skills", skillRouter);
 
 app.use((req, res, next) => {
